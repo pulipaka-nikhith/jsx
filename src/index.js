ab40176e96e67f58b19586c6c6354d81bdb9eb17
Nikhith Pulipaka <nikhith.p@surya-soft.com>
2022-01-26 07:07:47 +0530

    React basic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+// Import React and React DOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Create React component
+const App = ()=> {
+  return(
+   <div>
+  <label className="label" for="name">
+    Enter name :
+    </label>
+  <input id="name" type="text"/>
+  <button style={{backgroundColor:'blue',color:'white'}}> 
+  Submit
+  </button>
+</div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+};
+// Take the React component and show it on the page
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
