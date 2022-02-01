b8f7edc2fac6f21283419069102ca1ea41a259d8
Nikhith Pulipaka <nikhith.p@surya-soft.com>
2022-02-01 12:21:40 +0530

    React Js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import ReactDOM from 'react-dom';
 
 // Create React component
 const App = ()=> {
+  const buttonText ='Click Me !';
   return(
    <div>
-  <label className="label" for="name">
+  <label className="label" htmlFor="name">
     Enter name :
     </label>
   <input id="name" type="text"/>
   <button style={{backgroundColor:'blue',color:'white'}}> 
-  Submit
+     {buttonText}
   </button>
 </div>
 );
